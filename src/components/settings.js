25551043d260ff62a27a6551868c9e3b86b73811
hongsa <hsasung@gmail.com>
2017-03-01 16:16:30 +0900

    gitignore

--- src/components/settings.js
+++
@@ -1,9 +0,0 @@
-import * as firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCKRlYWPnAQ7e5bEGKOdgTctVpSdMGrT4Y",
-  authDomain: "react-native-sasung.firebaseapp.com",
-  databaseURL: "https://react-native-sasung.firebaseio.com",
-  storageBucket: "react-native-sasung.appspot.com"
-};
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
